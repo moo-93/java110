089398e02eae3468c03d847282e47dec0d6f114f
moo <msh9303@naver.com>
2018-09-04 19:29:53 +0900

    내부 클래스 관련 설명 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     //여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용한다.
-    static class Member{
+     static class Member{
         protected String name;
         protected String email;
         protected String password;
@@ -47,7 +47,14 @@ public class App {
 
     static void inputMembers() {
         while(true) {
+            //inner class 가져올때 외부 객체를 만들어준 후 내부객체를 가지고 옴다.
+            /*App a = new App();
+            App.Member m = a.new Member();*/
+            
+            //static nested inner class
+//            App.Member m = new App.Member();
             Member m = new Member();
+            
             System.out.print("이름 : ");
             m.setName(KeyIn.nextLine());
 
