91d3b9274c0cbc8249f68d7748731ae3856949b2
moo <msh9303@naver.com>
2018-09-04 14:57:13 +0900

    메서드 적용하여 코드 분리

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        String[] names = new String[100];
-        String[] emails = new String[100];
-        String[] passwords = new String[100];
-        int index = 0;
-        
-        Scanner KeyIn = new Scanner(System.in);
+    private static String[] names = new String[100];
+    private static String[] emails = new String[100];
+    private static String[] passwords = new String[100];
+    
+    private static int index = 0;
 
+    private static Scanner KeyIn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        inputMembers();
+        printMembers();
+        KeyIn.close();
+    }
+    
+    public static void printMembers() {
+        for(int i = 0; i < index; i++) {
+            System.out.printf("%s %s %s\n",names[i],emails[i],passwords[i]);
+        }
+    }
+
+    public static void inputMembers() {
         while(true) {
             System.out.print("이름 : ");
             names[index] = KeyIn.nextLine();
@@ -20,16 +33,10 @@ public class App {
             passwords[index] = KeyIn.nextLine();
 
             index++;
-            
+
             System.out.println("continue? (Y/n) ");
             String answer = KeyIn.nextLine();
             if(answer.toLowerCase().equals("n")) break;
         }
-        for(int i = 0; i < index; i++) {
-            System.out.printf("%s %s %s\n",names[i],emails[i],passwords[i]);
-        }
-        
-        KeyIn.close();
-        
     }
 }
