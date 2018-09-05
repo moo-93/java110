adfc8b1e206c1152c0dff74712f2a76fe424dd83
moo <msh9303@naver.com>
2018-09-05 11:02:22 +0900

    강사관리 기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -49,13 +49,39 @@ public class App {
         }
         public void setTel(String tel) {
             this.tel = tel;
-        }
+        }   
+    }
+    
+    static class Teacher extends Member{
+        protected String tel;
+        protected int pay;
+        protected String subjects;
         
+        public String getTel() {
+            return tel;
+        }
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+        public int getPay() {
+            return pay;
+        }
+        public void setPay(int pay) {
+            this.pay = pay;
+        }
+        public String getSubjects() {
+            return subjects;
+        }
+        public void setSubjects(String subjects) {
+            this.subjects = subjects;
+        }
     }
     
     static Student[] students = new Student[100];
+    static Teacher[] teachers = new Teacher[100];
 
-    static int index = 0;
+    static int studentIndex = 0;
+    static int teacherIndex = 0;
 
     static Scanner KeyIn = new Scanner(System.in);
 
@@ -66,6 +92,8 @@ public class App {
 
             if(menu.equals("1")) {
                 serviceStudentMenu();
+            } else if (menu.equals("2")) {
+              serviceTeacherMenu();  
             } else if (menu.equals("0")){
                 System.out.println("사용해주셔서 감사합니다!");
                 break;
@@ -85,6 +113,25 @@ public class App {
                 printStudents();
             } else if(command.equals("add")) {
                 inputStudents();
+            } else if(command.equals("quit")) {
+                break;
+            } else {
+                System.out.println("유효하지 않는 명령입니다.");
+            }
+        }
+    }
+    
+    private static void serviceTeacherMenu() {
+        while(true) {
+            System.out.println("[list] or [add] or [quit]");
+            System.out.print("강사 관리 > ");
+
+            String command = KeyIn.nextLine();
+
+            if (command.equals("list")) {    
+                printTeachers();
+            } else if(command.equals("add")) {
+                inputTeachers();
             } else if(command.equals("quit")) {
                 break;
             } else {
@@ -116,7 +163,7 @@ public class App {
     static void printStudents() {
         int cnt = 0;
         for(Student s : students) {
-            if(cnt++ == index) break;
+            if(cnt++ == studentIndex) break;
             System.out.printf("%s, %s, %s, %s, %b, %s\n"
                     ,s.getName()
                     ,s.getEmail()
@@ -124,6 +171,19 @@ public class App {
                     ,s.getSchool()
                     ,s.isWorking()
                     ,s.getTel());
+        }
+    }
+    static void printTeachers() {
+        int cnt = 0;
+        for(Teacher s : teachers) {
+            if(cnt++ == teacherIndex) break;
+            System.out.printf("%s, %s, %s, %s, %b, %s\n"
+                    ,s.getName()
+                    ,s.getEmail()
+                    ,s.getPassword()
+                    ,s.getTel()
+                    ,s.getPay()
+                    ,s.getSubjects());
         }
     }
     //merge test
@@ -146,19 +206,56 @@ public class App {
             System.out.print("암호 : ");
             m.setPassword(KeyIn.nextLine());
             
-            System.out.println("최종학력  : ");
+            System.out.print("최종학력  : ");
             m.setSchool(KeyIn.nextLine());
             
-            System.out.println("재직여부 : (true/false)");
+            System.out.print("재직여부 : (true/false)");
             m.setWorking(Boolean.parseBoolean(KeyIn.nextLine()));
             
-            System.out.println("전화번호 : ");
+            System.out.print("전화번호 : ");
             m.setTel(KeyIn.nextLine());
-            students[index++] = m;
+            students[studentIndex++] = m;
 
-            System.out.println("continue? (Y/n) ");
+            System.out.print("continue? (Y/n) ");
             String answer = KeyIn.nextLine();
             if(answer.toLowerCase().equals("n")) break;
         }
     }
+    
+    static void inputTeachers() {
+        while(true) {
+            //inner class 가져올때 외부 객체를 만들어준 후 내부객체를 가지고 옴다.
+            /*App a = new App();
+            App.Member m = a.new Member();*/
+
+            //static nested inner class
+            //App.Member m = new App.Member();
+            Teacher t = new Teacher();
+
+            System.out.print("이름 : ");
+            t.setName(KeyIn.nextLine());
+
+            System.out.print("이메일 : ");
+            t.setEmail(KeyIn.nextLine());
+
+            System.out.print("암호 : ");
+            t.setPassword(KeyIn.nextLine());
+            
+            System.out.print("전화번호  : ");
+            t.setTel(KeyIn.nextLine());
+            
+            System.out.print("시급 : ");
+            t.setPay(Integer.parseInt(KeyIn.nextLine()));
+            
+            System.out.print("강의과목 : ");
+            t.setSubjects(KeyIn.nextLine());
+            teachers[teacherIndex++] = t;
+
+            System.out.print("continue? (Y/n) ");
+            String answer = KeyIn.nextLine();
+            if(answer.toLowerCase().equals("n")) break;
+        }
+    }
+    
+    
 }
