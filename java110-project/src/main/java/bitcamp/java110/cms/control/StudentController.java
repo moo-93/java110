d3df1094d93bc4cb60af750404ee8e1366a7aa07
moo <msh9303@naver.com>
2018-09-05 16:30:09 +0900

    학생 목록 처리 별도 클래스로 분리

--- java110-project/src/main/java/bitcamp/java110/cms/control/StudentController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/StudentController.java
@@ -1,38 +1,13 @@
 package bitcamp.java110.cms.control;
 
 import java.util.Scanner;
-import bitcamp.java110.cms.domain.Member;
 
+import bitcamp.java110.cms.dao.StudentList;
+import bitcamp.java110.cms.domain.Student;
 
 public class StudentController {
-    static Student[] students = new Student[5];
-    static int studentIndex = 0;
+
     public static Scanner KeyIn;
-
-    static class Student extends Member{
-        protected String school;
-        protected boolean working;
-        protected String tel;
-
-        public String getSchool() {
-            return school;
-        }
-        public void setSchool(String school) {
-            this.school = school;
-        }
-        public boolean isWorking() {
-            return working;
-        }
-        public void setWorking(boolean working) {
-            this.working = working;
-        }
-        public String getTel() {
-            return tel;
-        }
-        public void setTel(String tel) {
-            this.tel = tel;
-        }   
-    }
 
     public static void serviceStudentMenu() {
         while(true) {
@@ -59,11 +34,10 @@ public class StudentController {
     }
 
     private static void printStudents() {
-        int cnt = 0;
-        for(Student s : students) {
-            if(cnt++ == studentIndex) break;
+        for(int i= 0; i< StudentList.size(); i++) {
+            Student s = StudentList.get(i);
             System.out.printf("%d : %s, %s, %s, %s, %b, %s\n"
-                    ,cnt-1
+                    ,i
                     ,s.getName()
                     ,s.getEmail()
                     ,s.getPassword()
@@ -75,12 +49,7 @@ public class StudentController {
 
     private static void inputStudents() {
         while(true) {
-            //inner class 가져올때 외부 객체를 만들어준 후 내부객체를 가지고 옴다.
-            /*App a = new App();
-            App.Member m = a.new Member();*/
 
-            //static nested inner class
-            //App.Member m = new App.Member();
             Student s = new Student();
 
             System.out.print("이름 : ");
@@ -101,11 +70,7 @@ public class StudentController {
             System.out.print("전화번호 : ");
             s.setTel(KeyIn.nextLine());
 
-            if(studentIndex == students.length) {
-                increaseStorage();
-            }
-
-            students[studentIndex++] = s;
+            StudentList.add(s);
 
             System.out.print("continue? (Y/n) ");
             String answer = KeyIn.nextLine();
@@ -113,28 +78,15 @@ public class StudentController {
         }
     }
 
-    private static void increaseStorage() {
-        Student[] newList = new Student[students.length+3];
-        for(int i = 0; i< students.length; i++) {
-            newList[i] = students[i];
-        }
-        students = newList;
-    }
-
     private static void deleteStudent() {
         System.out.print("삭제할 번호 : ");
         int num = Integer.parseInt(KeyIn.nextLine());
-        
-        if(num < 0 || num > studentIndex) {
+
+        if(num < 0 || num >= StudentList.size()) {
             System.out.println("무효한 번호입니다.");
             return;
         }
-
-        for(int i = num; i < studentIndex - 1; i++) {
-            students[i] = students[i+1];
-        }
-        studentIndex--;
-
+        StudentList.remove(num);
         System.out.println("삭제 완료");
     }
 
@@ -142,34 +94,36 @@ public class StudentController {
         System.out.print("조회할 번호 : ");
         int num = Integer.parseInt(KeyIn.nextLine());
 
-        if(num < 0 || num > studentIndex) {
+        if(num < 0 || num >= StudentList.size()) {
             System.out.println("무효한 번호입니다.");
             return;
         }
 
-        System.out.printf("이름 : %s\n", students[num].getName());
-        System.out.printf("이메일 : %s\n", students[num].getEmail());
-        System.out.printf("암호 : %s\n", students[num].getPassword());
-        System.out.printf("최종학력 : %s\n", students[num].getSchool());
-        System.out.printf("전화 : %s\n", students[num].getTel());
-        System.out.printf("재직여부 : %b\n", students[num].isWorking());
+        Student student = StudentList.get(num);
+
+        System.out.printf("이름 : %s\n", student.getName());
+        System.out.printf("이메일 : %s\n", student.getEmail());
+        System.out.printf("암호 : %s\n", student.getPassword());
+        System.out.printf("최종학력 : %s\n", student.getSchool());
+        System.out.printf("전화 : %s\n", student.getTel());
+        System.out.printf("재직여부 : %b\n", student.isWorking());
     }
 
-    /*static {
+    static {
         Student s = new Student();
         s.setName("a");
-        students[studentIndex++]=s;
+        StudentList.add(s);
         s = new Student();
         s.setName("b");
-        students[studentIndex++]=s;
+        StudentList.add(s);
         s = new Student();
         s.setName("c");
-        students[studentIndex++]=s;
+        StudentList.add(s);
         s = new Student();
         s.setName("d");
-        students[studentIndex++]=s;
+        StudentList.add(s);
         s = new Student();
         s.setName("e");
-        students[studentIndex++]=s;
-    }*/
+        StudentList.add(s);
+    }
 }
