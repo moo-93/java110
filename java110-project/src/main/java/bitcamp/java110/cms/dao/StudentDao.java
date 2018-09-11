d9ea9c14393edee50470b21aeb61774aacb4bb2b
moo <msh9303@naver.com>
2018-09-11 16:01:52 +0900

    StudentDao 인터페이스로 전환, Student 파일 입출력 추가

--- java110-project/src/main/java/bitcamp/java110/cms/dao/StudentDao.java
+++ java110-project/src/main/java/bitcamp/java110/cms/dao/StudentDao.java
@@ -1,45 +1,12 @@
 package bitcamp.java110.cms.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import bitcamp.java110.cms.annotation.Component;
 import bitcamp.java110.cms.domain.Student;
 
-@Component
-public class StudentDao {
-    
-    private List<Student> list = new ArrayList<>();
-    public int insert(Student student) {
-        for(Student item : list) {
-            if(item.getEmail().equals(student.getEmail())) {
-                return 0;
-            }
-        }
-        list.add(student);
-        return 1;
-    }
-    
-    public List<Student> findAll(){
-        return list;
-    }
-    
-    public Student findByEmail(String email) {
-        for(Student item : list) {
-            if(item.getEmail().equals(email)) {
-                return item;
-            }
-        }
-        return null;
-    }
-    
-    public int delete(String email) {
-        for(Student item : list) {
-            if(item.getEmail().equals(email)) {
-                list.remove(item);
-                return 1;
-            }
-        }
-        return 0;
-    }
+public interface StudentDao {
+    int insert(Student student);
+    public List<Student> findAll();
+    public Student findByEmail(String email); 
+    public int delete(String email);
 }
