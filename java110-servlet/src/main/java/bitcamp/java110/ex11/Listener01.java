b1bfe65848d5781044f27a3c0be0d93b9c15db9f
moo <msh9303@naver.com>
2018-10-02 11:43:39 +0900

    로그인에 세션 적용하기

---
+++ java110-servlet/src/main/java/bitcamp/java110/ex11/Listener01.java
@@ -0,0 +1,22 @@
+package bitcamp.java110.ex11;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class Listener01 implements HttpSessionListener{
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        // 세션이 생성되면 이 메서드가 호출된다.
+        System.out.printf("Listener01.sessionCreated():%s\n",
+                se.getSession().getId());
+    }
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        //세션이 소명되면 이 메서드가 호출된다.
+        System.out.printf("Listener01.sessionDestroyed():%s\n"
+                ,se.getSession().getId());
+    }
+}
